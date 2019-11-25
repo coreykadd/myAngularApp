6d2b4d585960f1f7889505d01a2f8aafd54b7c7a
cmangan <coreymangan22@live.ie>
2019-11-25 14:14:14 +0000

    removed console logs

--- front-end/src/app/modules/auth/login/login.component.ts
+++ front-end/src/app/modules/auth/login/login.component.ts
@@ -5,72 +5,65 @@ import { AuthService } from 'src/app/shared/services/auth.service';
 import { TokenStorageService } from 'src/app/shared/services/token-storage.service';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  loginForm: FormGroup;
-  formSubmitted = false;
-  loginValid: boolean;
-  rememberMe = false;
+    loginForm: FormGroup;
+    formSubmitted = false;
+    loginValid: boolean;
+    rememberMe = false;
 
-  constructor(private fb: FormBuilder, private router: Router, private authService: AuthService,
-              private tokenService: TokenStorageService) { }
+    constructor(private fb: FormBuilder, private router: Router, private authService: AuthService,
+                private tokenService: TokenStorageService) { }
 
-  ngOnInit() {
-    this.checkUserRole();
-    this.createLoginForm();
-  }
-
-  createLoginForm() {
-    this.loginForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required]
-    });
-  }
-
-  onSubmit() {
-    console.log(this.loginForm.value);
-    console.log(this.rememberMe);
-
-    this.formSubmitted = true;
-
-    if (this.loginForm.invalid) {
-      return;
-    } else {
-      this.authService.loginUser(this.loginForm.value)
-        .subscribe(
-          res => this.onLoginSuccess(res),
-          err => this.onLoginError(err)
-        );
+    ngOnInit() {
+        this.checkUserRole();
+        this.createLoginForm();
     }
-  }
 
-  onLoginSuccess(res) {
-    console.log('success ', res);
-    this.loginValid = true;
-    this.tokenService.saveTokens(res, this.rememberMe);
-    this.authService.setIsAuthenticated(true);
-    this.authService.getUserRole();
-    this.router.navigate(['home']);
-  }
-
-  onLoginError(err) {
-    this.loginValid = false;
-    console.log('error ', err);
-  }
-
-  checkUserRole() {
-    console.log('ROLEL ', this.authService.getUserRole());
-    if (this.authService.getUserRole()) {
-      this.authService.setIsAuthenticated(true);
-      this.router.navigate(['home']);
+    createLoginForm() {
+        this.loginForm = this.fb.group({
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', Validators.required]
+        });
     }
-  }
 
-  get form() {
-    return this.loginForm.controls;
-  }
+    onSubmit() {
+        this.formSubmitted = true;
+
+        if (this.loginForm.valid) {
+            this.authService.loginUser(this.loginForm.value)
+                .subscribe(
+                    res => this.onLoginSuccess(res),
+                    err => this.onLoginError(err)
+                );
+        }
+    }
+
+    onLoginSuccess(res) {
+        this.loginValid = true;
+        this.tokenService.saveTokens(res, this.rememberMe);
+        this.authService.setIsAuthenticated(true);
+        this.authService.getUserRole();
+        this.router.navigate(['home']);
+    }
+
+    onLoginError(err) {
+        this.loginValid = false;
+        console.log('error ', err);
+    }
+
+    checkUserRole() {
+        if (this.authService.getUserRole()) {
+            this.authService.setIsAuthenticated(true);
+            this.router.navigate(['home']);
+        }
+    }
+
+    get form() {
+        return this.loginForm.controls;
+    }
 
 }
