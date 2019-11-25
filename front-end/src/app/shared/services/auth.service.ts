ff58c696c893e4aae966c9ccf090a1f4e13d0cd9
cmangan <coreymangan22@live.ie>
2019-11-25 14:12:20 +0000

    added getUser method that gets user info from token

--- front-end/src/app/shared/services/auth.service.ts
+++ front-end/src/app/shared/services/auth.service.ts
@@ -6,57 +6,62 @@ import { TokenStorageService } from './token-storage.service';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthService {
-  private isAuthenticatedSubject = new BehaviorSubject<boolean>(false);
-  isAuthenticated = this.isAuthenticatedSubject.asObservable();
+    private isAuthenticatedSubject = new BehaviorSubject<boolean>(false);
+    isAuthenticated = this.isAuthenticatedSubject.asObservable();
 
-  constructor(private apiService: ApiService, private tokenService: TokenStorageService) { }
+    constructor(private apiService: ApiService, private tokenService: TokenStorageService) { }
 
-  registerUser(userCredentials: Login) {
-    return this.apiService.post(`${environment.users.register}`, userCredentials);
-  }
-
-  loginUser(userCredentials: Login) {
-    return this.apiService.post(`${environment.users.login}`, userCredentials);
-  }
-
-  getUserId() {
-    const token = this.getToken('access_token');
-    return token ? token.subject : undefined;
-  }
-
-  getUserRole() {
-    const token = this.getToken('access_token');
-    return token ? token.role : undefined;
-  }
-
-  getToken(type) {
-    const token = this.tokenService.getToken(type);
-
-    if (token && !this.isTokenExpired(token)) {
-      return token;
-    } else {
-      return null;
+    registerUser(userCredentials: Login) {
+        return this.apiService.post(`${environment.users.register}`, userCredentials);
     }
-  }
 
-  isTokenExpired(token) {
-    const date = new Date(token.exp * 1000);
-    return date ? (date.valueOf() < new Date().valueOf()) : false;
-  }
+    loginUser(userCredentials: Login) {
+        return this.apiService.post(`${environment.users.login}`, userCredentials);
+    }
 
-  setIsAuthenticated(isAuthenticated) {
-    this.isAuthenticatedSubject.next(isAuthenticated);
-  }
+    getUserId() {
+        const token = this.getToken('access_token');
+        return token ? token.subject : undefined;
+    }
 
-  getIsAuthenticated() {
-    return this.isAuthenticatedSubject.value;
-  }
+    getUserRole() {
+        const token = this.getToken('access_token');
+        return token ? token.role : undefined;
+    }
 
-  logoutUser() {
-    this.tokenService.destroyTokens();
-    this.setIsAuthenticated(false);
-  }
+    getUser() {
+        const token = this.getToken('access_token');
+        return token ? token.user : undefined;
+    }
+
+    getToken(type) {
+        const token = this.tokenService.getToken(type);
+
+        if (token && !this.isTokenExpired(token)) {
+            return token;
+        } else {
+            return null;
+        }
+    }
+
+    isTokenExpired(token) {
+        const date = new Date(token.exp * 1000);
+        return date ? (date.valueOf() < new Date().valueOf()) : false;
+    }
+
+    setIsAuthenticated(isAuthenticated) {
+        this.isAuthenticatedSubject.next(isAuthenticated);
+    }
+
+    getIsAuthenticated() {
+        return this.isAuthenticatedSubject.value;
+    }
+
+    logoutUser() {
+        this.tokenService.destroyTokens();
+        this.setIsAuthenticated(false);
+    }
 }
