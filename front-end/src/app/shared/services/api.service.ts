8db826e4b8b760c80f0f49fb509947fa458c0850
cmangan <coreymangan22@live.ie>
2019-11-25 14:15:13 +0000

    formatting and structure change

--- front-end/src/app/shared/services/api.service.ts
+++ front-end/src/app/shared/services/api.service.ts
@@ -5,38 +5,39 @@ import { environment } from 'src/environments/environment';
 import { catchError } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ApiService {
 
-  constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) { }
 
-  get(path: string, params: HttpParams = new HttpParams()): Observable<any> {
-    return this.http.get(`${environment.api_url}${path}`, { params, headers: { 'Content-Type': 'application/json' } })
-    .pipe(catchError(this.formatErrors));
-  }
+    get(path: string, params: HttpParams = new HttpParams()): Observable<any> {
+        return this.http.get(`${environment.api_url}${path}`, { params, headers: { 'Content-Type': 'application/json' } })
+            .pipe(catchError(this.formatErrors));
+    }
 
-  post(path: string, body: object): Observable<any> {
-    return this.http.post(`${environment.api_url}${path}`, JSON.stringify(body), { headers: { 'Content-Type': 'application/json' } })
-    .pipe(catchError(this.formatErrors));
-  }
+    post(path: string, body: object): Observable<any> {
+        return this.http.post(`${environment.api_url}${path}`, JSON.stringify(body), { headers: { 'Content-Type': 'application/json' } })
+            .pipe(catchError(this.formatErrors));
+    }
 
-  put(path: string, body: object): Observable<any> {
-    return this.http.put(`${environment.api_url}${path}`, JSON.stringify(body), { headers: { 'Content-Type': 'application/json' } })
-    .pipe(catchError(this.formatErrors));
-  }
+    put(path: string, body: object): Observable<any> {
+        return this.http.put(`${environment.api_url}${path}`, JSON.stringify(body), { headers: { 'Content-Type': 'application/json' } })
+            .pipe(catchError(this.formatErrors));
+    }
 
-  delete(path: string): Observable<any> {
-    return this.http.delete(`${environment.api_url}${path}`, { headers: {'Content-Type': 'application/json' } })
-    .pipe(catchError(this.formatErrors));
-  }
+    delete(path: string): Observable<any> {
+        return this.http.delete(`${environment.api_url}${path}`, { headers: { 'Content-Type': 'application/json' } })
+            .pipe(catchError(this.formatErrors));
+    }
 
-  getPdf(path: string): Observable < any > {
-    return this.http.get(`${environment.api_url}${path}`, {responseType: 'blob'})
-    .pipe(catchError(this.formatErrors));
-  }
+    getPdf(path: string): Observable<any> {
+        return this.http.get(`${environment.api_url}${path}`, { responseType: 'blob' })
+            .pipe(catchError(this.formatErrors));
+    }
 
-  private formatErrors(error: any) {
-    return throwError(error);
-  }
+    private formatErrors(error: any) {
+        console.log('>>>>>>> err');
+        return throwError(error);
+    }
 }
